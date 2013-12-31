dac28cdc6ee53a141790537546cc0920bc9eda32
Skyler Nesheim <skylernesheim@gmail.com>
2013-12-31 03:42:02 +0000

    change naming convention for files

---
+++ lib/elixir-blink.ex
@@ -0,0 +1,16 @@
+defmodule ElixirBlink do
+
+  def main(args) do 
+    GPIO.write_mode("13")
+    Enum.each 1..10, fn(_x) -> toggle() end   
+  end
+
+  defp toggle() do
+    GPIO.on("13")
+    :timer.sleep 500
+    GPIO.off("13")
+    :timer.sleep 500
+  end
+end
+
+
