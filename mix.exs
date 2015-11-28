689c7544f120fd88badb557824f7b15bc69c6dba
Skyler Nesheim <skyler@dwolla.com>
2015-11-28 15:49:40 -0600

    MASTER: add credo to project

--- mix.exs
+++ mix.exs
@@ -4,22 +4,18 @@ defmodule ElixirBlink.Mixfile do
   def project do
     [ app: :elixir_blink,
       version: "0.0.1",
+      elixir: "~> 1.1",
       deps: deps ]
   end
 
-  # Configuration for the OTP application
   def application do
     []
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
-  #
-  # To specify particular versions, regardless of the tag, do:
-  # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
   defp deps do
     [
-      { :mock, github: "jjh42/mock" }
+      { :mock, github: "jjh42/mock" },
+      {:credo, "~> 0.1.9", only: [:dev, :test]}
     ]
   end
 end
